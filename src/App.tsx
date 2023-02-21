9c7fcbb438fcdcd94a2f374730b7a65574148744
Roman Kamenivskyi <rk@wwwest.solutions>
2023-02-21 14:24:54 +0200

    add func for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { routes } from "app/routes";
 import { Layout } from "components/Layout";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    async function testReq() {
+      console.log("before fetch");
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((r) => r.json())
+        .then((r) => {
+          console.log("data: ", r);
+        });
+    }
+    testReq();
+  }, []);
   return (
     <div className="App">
       <Layout>{routes}</Layout>
