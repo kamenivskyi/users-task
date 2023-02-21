0707f01a831d0f3484d107f29b28ffa9123aa2aa
Roman Kamenivskyi <rk@wwwest.solutions>
2023-02-21 12:20:04 +0200

    implement getting users and user posts page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import Button from "@mui/material/Button";
 import "./App.css";
 import { routes } from "app/routes";
+import { Layout } from "components/Layout";
 
 function App() {
-  return <div className="App">{routes}</div>;
+  return (
+    <div className="App">
+      <Layout>{routes}</Layout>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/features/users/types.ts
@@ -0,0 +1,35 @@
+export interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+export interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
