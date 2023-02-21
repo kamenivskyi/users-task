53b2adaf09edd771e8cb9088239e425f854c730c
Roman Kamenivskyi <rk@wwwest.solutions>
2023-02-21 14:31:31 +0200

    delete unnecessary func, change browser router to hash

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,6 @@ import { Layout } from "components/Layout";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    async function testReq() {
-      console.log("before fetch");
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then((r) => r.json())
-        .then((r) => {
-          console.log("data: ", r);
-        });
-    }
-    testReq();
-  }, []);
   return (
     <div className="App">
       <Layout>{routes}</Layout>
