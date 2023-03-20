30f703b83f5a432c7691356851de8dbabfd94d9b
Roman Kamenivskyi <rk@wwwest.solutions>
2023-03-20 18:42:28 +0200

    implement use animated list hook

--- src/features/users/pages/UserPosts.tsx
+++ src/features/users/pages/UserPosts.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import gsap from "gsap";
+import React, { useEffect, useRef } from "react";
 import { List, ListItem, ListItemText } from "@mui/material";
 import Error from "components/Error";
 import Spinner from "components/Spinner";
 import { useParams } from "react-router-dom";
 import { useGetUserPostsQuery } from "../usersAPI";
+import { useAnimatedList } from "hooks";
+
+const postClass = "user-posts";
 
 export const UserPosts = () => {
   const param: any = useParams();
 
   const { data, isFetching, isError } = useGetUserPostsQuery(param.userId);
+
+  useAnimatedList(data, postClass);
+
   return (
     <>
       {isFetching && <Spinner />}
@@ -16,8 +23,8 @@ export const UserPosts = () => {
       {isError && <Error />}
 
       <List dense={false}>
-        {data?.map((item) => (
-          <ListItem key={item.id}>
+        {data?.map((item, idx) => (
+          <ListItem className={postClass} key={item.id}>
             <ListItemText
               primary={item.title}
               secondary={item.body ? item.body : null}
